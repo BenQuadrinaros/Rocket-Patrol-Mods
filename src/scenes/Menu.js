e2f48515adb9a50a400afb81ae460cdf4b89fc9c
Ben Rowland <rowland.ben.j@gmail.com>
2020-04-09 10:02:47 -0700

    This should work

---
+++ src/scenes/Menu.js
@@ -0,0 +1,15 @@
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene");
+    }
+    
+    create() {
+        //display menu text
+        this.add.text(20,20,"Rocket Patrol Menu");
+
+        //launches next scene
+        this.scene.start("playScene");
+    }
+}
+
+//
